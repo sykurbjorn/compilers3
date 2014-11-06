23bcee67c71fdfe4137faded9b2939e6f0edde51
Sigurbjorn Kristjansson <sigurbjornk@gmail.com>
2014-11-06 18:02:59 +0000

    dicksauce

--- CodeGenerator.java
+++ CodeGenerator.java
@@ -2,5 +2,7 @@
  * Created by siddi on 05/11/14.
  */
 public class CodeGenerator {
-    void generate(TacCode, SymbolTableEntry param1, SymbolTableEntry param2, SymbolTableEntry entry);
+    void generate(TacCode, SymbolTableEntry param1, SymbolTableEntry param2, SymbolTableEntry entry){
+        // stuff
+    };
 }
