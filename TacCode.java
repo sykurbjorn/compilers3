bdc05daa5c7e7b756bfc898ab4bfcfbf0b5c5b93
Sigurbjorn Kristjansson <sigurbjornk@gmail.com>
2014-11-06 21:20:27 +0000

    dicksauce

---
+++ TacCode.java
@@ -0,0 +1,12 @@
+/**
+ * Created by siddi on 06/11/14.
+ */
+public enum TacCode {
+        LABEL, UMINUS, ASSIGN,
+        ADD, SUB, MULT, DIVIDE, DIV, MOD,
+        OR, AND, NOT,
+        LT, LE, GT, GE, EQ, NE,
+        GOTO, CALL,
+        APARAM, FPARAM,
+        VAR, RETURN, NOOP
+}
